a30e9bbf13aa05fff69c3c6ddf6349874bc6552a
David Morgan <dmorgan81@gmail.com>
2017-12-14 09:45:22 -0500

    Time layer

--- src/c/main.c
+++ src/c/main.c
@@ -1,9 +1,23 @@
 #include <pebble.h>
 #include <pebble-layout/pebble-layout.h>
+#include <pebble-events/pebble-events.h>
 #include "logging.h"
 
 static Window *s_window;
 static Layout *s_layout;
+static EventHandle s_event_handle;
+
+static void prv_tick_handler(struct tm *tick_time, TimeUnits units_changed, void *context) {
+    logf();
+    TextLayer *text_layer = (TextLayer *) context;
+    char *buf = (char *) text_layer_get_text(text_layer);
+
+    char s[8];
+    strftime(s, sizeof(s), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+    memcpy(buf, s, sizeof(s));
+
+    text_layer_set_text((TextLayer *) context, buf);
+}
 
 static void prv_window_load(Window *window) {
     logf();
@@ -13,10 +27,16 @@ static void prv_window_load(Window *window) {
 
     layout_parse(s_layout, RESOURCE_ID_LAYOUT);
     layer_add_child(window_get_root_layer(window), layout_get_root_layer(s_layout));
+
+    TextLayer *time_layer = layout_find_by_id(s_layout, "time");
+    time_t now = time(NULL);
+    prv_tick_handler(localtime(&now), MINUTE_UNIT, time_layer);
+    s_event_handle = events_tick_timer_service_subscribe_context(MINUTE_UNIT, prv_tick_handler, time_layer);
 }
 
 static void prv_window_unload(Window *window) {
     logf();
+    events_tick_timer_service_unsubscribe(s_event_handle);
     layout_destroy(s_layout);
 }
 
